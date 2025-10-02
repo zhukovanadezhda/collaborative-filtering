f2fef10b16c9a9163c4c1c87e878f5075a793551
nadia <nadiajuckova@gmail.com>
2025-10-02 22:32:57 +0200

    Update the main script

--- generate.py
+++ generate.py
@@ -1,8 +1,9 @@
-
 import numpy as np
 import os
 from tqdm import tqdm, trange
 import argparse
+
+from scripts.tools import complete_ratings, compute_rmse
 
 
 if __name__ == '__main__':
@@ -24,10 +25,20 @@ if __name__ == '__main__':
     table = np.load(args.name) ## DO NOT CHANGE THIS LINE
     print('Ratings Loaded.')
     
+    # Define the parameters
+    params = {
+        "n_factors": 99,
+        "n_iters": 41,
+        "reg": 0.795,
+        "random_state": 42
+    }
 
-    # Any method you want
-    average = np.nanmean(table)
-    table = np.nan_to_num(table, nan=average)
+    # Complete the ratings table
+    table = complete_ratings(
+        train_path="data/ratings_train.npy",
+        test_path="data/ratings_test.npy",
+        params=params
+        )
 
     # Save the completed table 
     np.save("output.npy", table) ## DO NOT CHANGE THIS LINE
